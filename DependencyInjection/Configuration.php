86fd111eef6b2f84e84cb2bfb7e0f70589d85451
HETRU Mathieu <mathieu.hetru@laposte.net>
2020-03-01 23:51:55 +0100

    modif for sf5.0

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('l3_security');
+        $treeBuilder = new TreeBuilder('l3_security');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
